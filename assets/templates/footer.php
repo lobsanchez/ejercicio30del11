4ccd6b185ccf8139422d89c9061e9518b9d472c2
Luis Óscar Bueno Sánchez <lobsanchez@hotmail.com>
2023-12-13 20:10:01 +0100

    Footer terminado en clase al 95%

--- assets/templates/footer.php
+++ assets/templates/footer.php
@@ -27,20 +27,23 @@
                 <button>Order</button>
             </div>
         </div>
+        <div class="logo-footer">
+            <img src="assets\images\logoFooter.svg" alt="">
+        </div>
+        <ul>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">Pricing Plans</a></li>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">Blog</a></li>
+        </ul>
+        <div class="social">
+            <a href="#"><img src="assets\images\Facebook.svg" alt="" srcset=""></a>
+            <a href="#"><img src="assets\images\Twitter.svg" alt="" srcset=""></a>
+            <a href="#"><img src="assets\images\Instagram.svg" alt="" srcset=""></a>    
+        </div>
     </section>
-    <div class="logo-footer">
-        <img src="assets\images\logoFooter.svg" alt="">
-    </div>
-    <ul>
-        <li><a href="#">Products</a></li>
-        <li><a href="#">Pricing Plans</a></li>
-        <li><a href="#">FAQ</a></li>
-        <li><a href="#">Blog</a></li>
-    </ul>
-    <div>
-        <a href="#"><img src="assets\images\Facebook.svg" alt="" srcset=""></a>
-        <a href="#"><img src="assets\images\Twitter.svg" alt="" srcset=""></a>
-        <a href="#"><img src="assets\images\Instagram.svg" alt="" srcset=""></a>    
-    </div>
+   
+        
+   
 </footer>
 
