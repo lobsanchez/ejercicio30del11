cf68f3aa849d1d4dbb490db16413c35886b6960e
Oscar Bueno <lobsanchez@hotmail.com>
2023-12-17 20:13:24 +0100

    Terminado, sólo falta borde eimagen. Cuando controle más

--- assets/templates/header.php
+++ assets/templates/header.php
@@ -1,33 +1,33 @@
 <header>
-    
-    <div class="logo">
-        <img src="assets\images\logo.png" alt="">
-    </div>
-    <nav>
-        <ul>
-            <li><a href="#">Products</a></li>
-            <li><a href="#">Pricing</a></li>
-            <li><a href="#">FAQ</a></li>
-            <li><a href="#">Blog</a></li>
-            <li><a href="#">Blog</a></li>
-        </ul>
-    </nav>
-    <div class="nav-right">
-        <button class="no-border">Sign in</button>
-        <button class="border">Sign Up</button>
-    </div>
-    
-    <!-- <section class="section-1"> -->
-    
-    <div class="title-hero">
-        <h1>Monitor your business on real-time dashboard</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. </p>
-        <button class="pink">Try for free</button>
-    </div>
-    <div class="img-hero">
-        <img src="assets\images\main-screenDoble.svg" alt="Imagen doble">
-    </div>
-    <!-- </section> -->
 
+        <div class="logo">
+            <img src="assets\images\logo.png" alt="">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Products</a></li>
+                <li><a href="#">Pricing</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Blog</a></li>
+            </ul>
+        </nav>
+        <div class="nav-right">
+            <button class="no-border">Sign in</button>
+            <button class="border">Sign Up</button>
+        </div>
+        
+        <!-- <section class="section-1"> -->
+        
+        <div class="title-hero">
+            <h1>Monitor your business on real-time dashboard</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. </p>
+            <button class="pink">Try for free</button>
+        </div>
+        <div class="img-hero">
+            <img src="assets\images\main-screenDoble.svg" alt="Imagen doble">
+        </div>
+        <!-- </section> -->
+    
 </header>
 
